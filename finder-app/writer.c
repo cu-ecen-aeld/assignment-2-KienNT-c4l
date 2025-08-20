1178f0c48d559dcc065e2d5ae3a68c817ca998af
Nguyễn Trung Kiên <112324296+KienNT-c4l@users.noreply.github.com>
2025-08-20 16:51:56 +0700

    Update writer.c

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <syslog.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
 
 int main(int argc, char *argv[]) {
     // Mở syslog với facility LOG_USER
@@ -15,25 +19,27 @@ int main(int argc, char *argv[]) {
     const char *writefile = argv[1];
     const char *writestr  = argv[2];
 
-    // Mở file để ghi đè
-    FILE *fp = fopen(writefile, "w");
-    if (!fp) {
-        syslog(LOG_ERR, "Could not open file %s for writing", writefile);
+    // Dùng system call open()
+    int fd = open(writefile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (fd == -1) {
+        syslog(LOG_ERR, "Could not open file %s: %s", writefile, strerror(errno));
         closelog();
         return 1;
     }
 
-    // Ghi nội dung vào file
-    if (fputs(writestr, fp) == EOF) {
-        syslog(LOG_ERR, "Failed to write to file %s", writefile);
-        fclose(fp);
+    // Dùng system call write()
+    ssize_t n = write(fd, writestr, strlen(writestr));
+    if (n == -1) {
+        syslog(LOG_ERR, "Failed to write to file %s: %s", writefile, strerror(errno));
+        close(fd);
         closelog();
         return 1;
     }
 
     syslog(LOG_DEBUG, "Writing %s to %s", writestr, writefile);
 
-    fclose(fp);
+    // Đóng file với system call close()
+    close(fd);
     closelog();
     return 0;
 }
